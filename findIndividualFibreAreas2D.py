bda1858ea36f7e931d1687c83f6a5b0ee317e5b8
Jake <jw01170@surrey.ac.uk>
2021-02-25 16:25:01 +0000

    Mass update of code
    Adding 3D image code
    Adding basic 2D area stats code per slice
    Adding individual fibre 2D area analysis

---
+++ findIndividualFibreAreas2D.py
@@ -0,0 +1,86 @@
+import numpy as np
+import cv2
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+pixelSize = 1.4
+
+# load image and convert to 8 bit binary (byte)
+img = np.load('./3DImageStackBinary.npy')
+img = img.astype(np.uint8)
+
+# number of slices in the image (i.e. z-axis length)
+# area array will store array of all fibre areas
+# component count array simply stores number of fibres in each slice
+nSlices = img.shape[0]
+areaList = []
+componentCountArray = np.zeros(nSlices)
+
+# TODO take code and loop over all slices
+# generate area stats per each fibre,
+# shouldn't need a 3d Array
+# then just do histogram of stats
+
+for slice in range(nSlices):
+    # get slice and find number of individual fibres and an array of where each component is
+    sliceImage = img[slice]
+    nComponents, componentsArray = cv2.connectedComponents(sliceImage)
+
+    componentCountArray[slice] = nComponents
+
+    sliceCountArray = np.zeros(nComponents)
+    sliceAreaArray = np.zeros(nComponents)
+
+    # for each component count the number of pixels that make up that component i.e. fibre size
+    for component in range(nComponents):
+        sliceCountArray[component] = np.count_nonzero(componentsArray == component)
+
+    # find area of all of these components
+    sliceAreaArray = sliceCountArray * pixelSize ** 2
+
+    areaList.append(sliceAreaArray)
+
+# concatenate array list into single array and then flatten to 1D
+areaArray = np.concatenate(areaList).flatten()
+print('areaArray shape, should be same as nComponents', areaArray.shape)
+print('values range between ', np.amin(areaArray), 'um^2 and ', np.amax(areaArray), ' um^2')
+print('this corresponds to ', np.amin(areaArray)/(1.4**2), ' pixels and ', np.amax(areaArray)/(1.4**2), ' pixels')
+print('sum of nComponents (no. fibres)', np.sum(componentCountArray))
+fig = make_subplots(rows=2, cols=1)
+
+fig.add_traces(
+    go.Histogram(
+        x=areaArray,
+        name='Fibre Area um^2'
+    ),
+    rows=1, cols=1
+)
+
+fig.add_traces(
+    go.Histogram(
+        x=componentCountArray,
+        name='No. Fibres in a slice'
+    ),
+    rows=2, cols=1
+)
+
+fig.show()
+
+## This function can be used to show components in a slice, labelled with an individual
+## colour, keeping for future reference
+# def imshow_components(componentsArray):
+#     # Map component labels to hue val
+#     label_hue = np.uint8(179*componentsArray/np.max(componentsArray))
+#     blank_ch = 255*np.ones_like(label_hue)
+#     labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
+
+#     # cvt to BGR for display
+#     labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
+
+#     # set bg label to black
+#     labeled_img[label_hue==0] = 0
+
+#     cv2.imshow('labeled.png', labeled_img)
+#     cv2.waitKey()
+
+# imshow_components(componentsArray)
---
+++ findLayerAreaStats2D.py
@@ -0,0 +1,157 @@
+import numpy as np
+import cv2
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# load image as pixel array
+image = np.load('./3DImageStackBinary.npy')
+
+# pixel size in micrometres
+pixelSize = 1.4
+print('Each pixel has side length', pixelSize, ' micrometres a side')
+
+# generate stats function
+def genStats(imageSlice):
+    imageSlice = imageSlice.astype(np.uint8)
+
+    retVal, labels = cv2.connectedComponents(imageSlice)
+
+    numFibres = retVal - 1
+    numFibrePixels = 0
+    flatArray = imageSlice.flatten()
+
+    for i in range(1, retVal):
+        pts = np.where(labels == i)
+        labels[pts] = 1
+        numFibrePixels = numFibrePixels + len(pts[0])
+
+    fracFibres = numFibrePixels / len(flatArray)
+
+    meanFibreArea = (numFibrePixels / numFibres) * pixelSize **2
+
+    meanFibreDiameter = np.sqrt(meanFibreArea)
+
+    estChannelWidth = meanFibreDiameter / np.sqrt(fracFibres)
+
+    return fracFibres, meanFibreArea, meanFibreDiameter, estChannelWidth
+
+
+# z plane
+nSlicesZ = image.shape[0]
+print(nSlicesZ)
+
+fracFibresSlicesZ = np.zeros(nSlicesZ)
+meanFibreAreaSlicesZ = np.zeros(nSlicesZ)
+meanFibreDiameterSlicesZ = np.zeros(nSlicesZ)
+estChannelWidthSlicesZ = np.zeros(nSlicesZ)
+
+for imageNo in range(nSlicesZ):
+    imageSlice = image[imageNo]
+    fracFibres, meanFibreArea, meanFibreDiameter, estChannelWidth = genStats(imageSlice)
+
+    fracFibresSlicesZ[imageNo] = fracFibres
+    meanFibreAreaSlicesZ[imageNo] = meanFibreArea
+    meanFibreDiameterSlicesZ[imageNo] = meanFibreDiameter
+    estChannelWidthSlicesZ[imageNo] = estChannelWidth
+
+
+
+# x plane
+nSlicesX = image.shape[1]
+print(nSlicesX)
+
+fracFibresSlicesX = np.zeros(nSlicesX)
+meanFibreAreaSlicesX = np.zeros(nSlicesX)
+meanFibreDiameterSlicesX = np.zeros(nSlicesX)
+estChannelWidthSlicesX = np.zeros(nSlicesX)
+
+for imageNo in range(nSlicesX):
+    imageSlice = image[0:, imageNo, 0:]
+    fracFibres, meanFibreArea, meanFibreDiameter, estChannelWidth = genStats(imageSlice)
+
+    fracFibresSlicesX[imageNo] = fracFibres
+    meanFibreAreaSlicesX[imageNo] = meanFibreArea
+    meanFibreDiameterSlicesX[imageNo] = meanFibreDiameter
+    estChannelWidthSlicesX[imageNo] = estChannelWidth
+
+# y plane
+nSlicesY = image.shape[2]
+print(nSlicesY)
+
+fracFibresSlicesY = np.zeros(nSlicesY)
+meanFibreAreaSlicesY = np.zeros(nSlicesY)
+meanFibreDiameterSlicesY = np.zeros(nSlicesY)
+estChannelWidthSlicesY = np.zeros(nSlicesY)
+
+for imageNo in range(nSlicesY):
+    imageSlice = image[0:, imageNo, 0:]
+    fracFibres, meanFibreArea, meanFibreDiameter, estChannelWidth = genStats(imageSlice)
+
+    fracFibresSlicesY[imageNo] = fracFibres
+    meanFibreAreaSlicesY[imageNo] = meanFibreArea
+    meanFibreDiameterSlicesY[imageNo] = meanFibreDiameter
+    estChannelWidthSlicesY[imageNo] = estChannelWidth
+
+# TODO : generate bins and number of items in each bin to plot inside histogram
+
+dataZ = {
+    'FibreFraction': fracFibresSlicesZ, 
+    'MeanFibreArea': meanFibreAreaSlicesZ,
+    'MeanFibreDiameter': meanFibreDiameterSlicesZ,
+    'EstimatedChannelWidth': estChannelWidthSlicesZ
+    }
+
+dataX = {
+    'FibreFraction': fracFibresSlicesX, 
+    'MeanFibreArea': meanFibreAreaSlicesX,
+    'MeanFibreDiameter': meanFibreDiameterSlicesX,
+    'EstimatedChannelWidth': estChannelWidthSlicesX
+    }
+
+dataY = {
+    'FibreFraction': fracFibresSlicesY, 
+    'MeanFibreArea': meanFibreAreaSlicesY,
+    'MeanFibreDiameter': meanFibreDiameterSlicesY,
+    'EstimatedChannelWidth': estChannelWidthSlicesY
+    }
+
+dataFrameZ = pd.DataFrame(data = dataZ)
+dataFrameX = pd.DataFrame(data = dataX)
+dataFrameY = pd.DataFrame(data = dataY)
+
+fig = make_subplots(rows=2, cols=2)
+
+fig.add_traces(
+    go.Histogram(
+        x=dataFrameY['FibreFraction'],
+        name='Fibre Fraction'
+        ),
+    rows=1, cols=1
+)
+
+fig.add_traces(
+    go.Histogram(
+        x=dataFrameY['MeanFibreArea'],
+        name='Mean Fibre Area'),
+    rows=1, cols=2
+)
+
+fig.add_traces(
+    go.Histogram(
+        x=dataFrameY['MeanFibreDiameter'],
+        name='Mean Fibre Diameter'
+    ),
+    rows=2, cols=1
+)
+
+fig.add_traces(
+    go.Histogram(
+        x=dataFrameY['EstimatedChannelWidth'],
+        name='Estimated Channel Width'
+        ),
+    rows=2, cols=2
+)
+
+fig.show()
